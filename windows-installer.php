51ab224ae8f53bb505ee2d6ef576c11ad6b5fa34
The Juan Who Code <caserjan@gmail.com>
2013-10-24 22:52:20 +0800

    Updated directory

---
+++ windows-installer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Not-Another Model-Controller Framework
+ * 
+ * NAMCF CLI
+ *
+ * @version b0.1
+ * @author The Juan Who Code <caserjan@gmail.com>
+ * @package core
+ */
+
+if(){
+}
+
+?>
